803ffb28ca39382ad3ae820c149d6330ae408b9d
Aaron Myatt <aaronmyatt@gmail.com>
2026-01-03 19:10:52 +0800

    ensure vscode extension has the correct backend url embedded in its bundle

--- rollup.config.js
+++ rollup.config.js
@@ -23,7 +23,7 @@ export default [
     plugins: [
       replace({
         'process.env.NODE_ENV': JSON.stringify(production ? 'production' : 'development'),
-        'process.env.API_BASE_URL': JSON.stringify(production ? '/api/v1' : 'http://localhost:4000/api/v1'),
+        'process.env.API_BASE_URL': JSON.stringify(production ? 'https://waystation.aaronmyatt.com/api/v1' : 'http://localhost:4000/api/v1'),
         preventAssignment: true,
       }),
       resolve({
